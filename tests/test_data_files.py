29209d13ea8508ac261f54ce57d069f1ae6f7864
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-03-05 14:37:54 -0300

    tests: add test for user name data file

--- tests/test_data_files.py
+++ tests/test_data_files.py
@@ -6,7 +6,8 @@ import unittest
 from constants import APP_NAME
 from game import Score
 from utils import (
-    HIGHSCORES_FILENAME, get_highscores, write_highscores, get_user_name,
+    HIGHSCORES_FILENAME, USER_NAME_FILENAME, get_highscores, write_highscores,
+    get_user_name,
     write_user_name
 )
 
@@ -45,6 +46,37 @@ class DataFilesTestCase(unittest.TestCase):
 
         if temp:
             # Write the highscores back
+            try:
+                with open(filename, 'wt') as f:
+                    f.write(temp)
+            except IOError:
+                raise Exception(
+                    f"Couldn't write back {filename} after testing"
+                )
+
+    def test_user_name(self):
+        folder = pyglet.resource.get_data_path(APP_NAME)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        filename = os.path.join(folder, USER_NAME_FILENAME)
+        temp = None
+        if os.path.exists(filename):
+            # If there's a user name already, temporarily save it
+            try:
+                with open(filename, 'rt') as f:
+                    temp = f.read()
+            except IOError:
+                raise Exception(
+                    f"file {filename} couldn't been open for testing user name"
+                )
+
+        user_name = "Test user name"
+        write_user_name(user_name)
+        user_name_from_file = get_user_name()
+        self.assertEqual(user_name, user_name_from_file)
+
+        if temp:
+            # Write back the user name to the file
             try:
                 with open(filename, 'wt') as f:
                     f.write(temp)
