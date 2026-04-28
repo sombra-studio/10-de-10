d7254a996b7fdd71a066597455313d0a20fed265
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-04-28 16:33:52 -0400

    misc: add pyinstaller

---
+++ build_command.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+pyinstaller --windowed --noconfirm \
+    --clean \
+    --add-data="assets/:.assets/" \
+    --add-data="locales/:.locales/" \
+    --collect-data=pudu_ui \
+    --icon=./assets/imgs/line-game-logo.png \
+    --name="10 de 10" \
+    --onefile \
+    main.py
+
+
