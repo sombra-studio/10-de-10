b512b3b32f9f7b771eda2670d607eca986397af8
jesus <1151071+HenrYxZ@users.noreply.github.com>
2026-04-29 18:31:44 -0400

    fix: remove . before folder name

--- build_command.sh
+++ build_command.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 pyinstaller --windowed --noconfirm \
-    --clean \
-    --add-data="assets/:.assets/" \
-    --add-data="locales/:.locales/" \
+    --add-data="assets:assets" \
+    --add-data="locales:locales" \
     --collect-data=pudu_ui \
     --icon=./assets/imgs/line-game-logo.png \
     --name="10 de 10" \
-    --onefile \
     main.py
 
 
