9a9a633e3d35ca52724849677835639c6c5564bc
henry <1151071+HenrYxZ@users.noreply.github.com>
2026-05-09 21:11:32 -0400

    misc: mac app compiling and closing unexpectedly

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-# Compilation mode, support OS-specific options
-# nuitka-project: --mode=standalone
+# --- Build Configuration ---
+# nuitka-project: --standalone
+# nuitka-project: --product-name="10 de 10"
 
-# Set variables dynamically and use them later, e.g., for Windows metadata
+# --- Windows ---
 # nuitka-project-if: {OS} == "Windows":
-#    nuitka-project-set: MY_VERSION = __import__("line-game").__version__
-#    nuitka-project: --file-version={MY_VERSION}
 #    nuitka-project: --windows-icon-from-ico={MAIN_DIRECTORY}/assets/imgs/line-game-logo.png
 
+# --- macOS ---
 # nuitka-project-if: {OS} == "Darwin":
+#    nuitka-project: --macos-create-app-bundle
 #    nuitka-project: --macos-app-icon={MAIN_DIRECTORY}/assets/imgs/line-game-logo.png
-#    nuitka-project: --include-frameworks=AppKit,CoreVideo,CoreFoundation,OpenGL,QuartzCore
 
-# nuitka-project: --product-name="10 de 10"
-# nuitka-project: --include-data-dir={MAIN_DIRECTORY}/assets=.
-# nuitka-project: --include-data-dir={MAIN_DIRECTORY}/locales=.
+# --- Files & Packages ---
+# nuitka-project: --include-data-dir={MAIN_DIRECTORY}/assets=assets
+# nuitka-project: --include-data-dir={MAIN_DIRECTORY}/locales=locales
 # nuitka-project: --include-package-data=pudu_ui
+# nuitka-project: --include-package=pyglet
 
-# Don't depend on tkinter
+# --- Exclusions ---
 # nuitka-project: --nofollow-import-to=tkinter
 # nuitka-project: --nofollow-import-to=_tkinter
-
-# Other dependencies we don't want
 # nuitka-project: --nofollow-import-to=unittest
 # nuitka-project: --nofollow-import-to=pydoc
+
 
 from app import GameApp
 
